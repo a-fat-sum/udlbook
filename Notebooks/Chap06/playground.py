40878705ba88253e13505deb38603ebc20b8b3c0
Mustafa Fanaswala <mufanas@microsoft.com>
2024-06-10 08:01:32 -0700

    Finish Chapters 7, 8

--- Notebooks/Chap06/playground.py
+++ Notebooks/Chap06/playground.py
@@ -1,14 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-n_steps = 81
-batch_size = 5
-alpha = 0.6
-beta = 0.6
-momentum = np.zeros([2,1])
-phi_all = np.zeros((2,n_steps+1))
-phi_all[0,0] = -1.5
-phi_all[1,0] = 6.5
-x = phi_all[:,3]
-y = np.zeros([2,1])
-z = x - y 
-k = 5
+x = np.linspace(0, 1000000)
+y = np.log(1+x)
+
+plt.figure()
+plt.plot(x,y)
+plt.show()
+
+z = 10
